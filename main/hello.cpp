42e30c14e0561a3a9be4334d2c751fec5b4f882d
NitishKumar-student <nitishdoddamani098@gmail.com>
2025-03-15 00:09:16 +0530

    Update hello.cpp with webhook

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "Hello, Webhook Test from GitHub! \n";
+  cout << "Hello, Webhook Test from GitHub \n";
   return 0;
 }
