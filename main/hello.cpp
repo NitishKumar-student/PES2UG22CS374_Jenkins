647e966def31bc881a512b0c29a77240fb80a806
NitishKumar-student <nitishdoddamani098@gmail.com>
2025-03-14 23:25:38 +0530

    "Updated hello.cpp to test Jenkins webhook"

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout << "Hello, Webhook Test from GitHub!" << endl;
   return 0;
 }
